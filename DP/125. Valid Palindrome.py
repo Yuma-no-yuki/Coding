5e357367d71c5d225cb585f66a04f404f27848e7
Ivan Tseng <73100384+Yuma-no-yuki@users.noreply.github.com>
2024-01-08 13:55:07 +0800

    Update 125. Valid Palindrome.py

--- DP/125. Valid Palindrome.py
+++ DP/125. Valid Palindrome.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
         s=s.lower()
         l=""
         for i in s:
@@ -11,11 +7,7 @@ class Solution(object):
                 l+=i
             elif 48<=ord(i) and ord(i)<=57:
                 l+=i
-        print(l)
         for i in range(len(l)):
             if l[i]!=l[-(i+1)]:
                 return False
         return True
-        # for i in range(int((len(s)-1)/2)):
-        #     print(s[i],s[-(i+1)])
-print(Solution().isPalindrome("0P"))
