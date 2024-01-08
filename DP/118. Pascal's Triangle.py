c390abc10a0da8233223d9902c863f0060587aea
Ivan Tseng <73100384+Yuma-no-yuki@users.noreply.github.com>
2024-01-08 13:56:58 +0800

    Update 118. Pascal's Triangle.py

--- DP/118. Pascal's Triangle.py
+++ DP/118. Pascal's Triangle.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def generate(self, numRows):
-        """
-        :type numRows: int
-        :rtype: List[List[int]]
-        """
         tri = []
         for i in range(numRows):
             row = [1] * (i+1)
@@ -11,5 +7,6 @@ class Solution(object):
                 for j in range(1,i):
                     row[j] = tri[i-1][j] + tri[i-1][j-1]
             tri.append(row)
-        print(tri)
-Solution().generate(5)
+        return tri
+
+        
