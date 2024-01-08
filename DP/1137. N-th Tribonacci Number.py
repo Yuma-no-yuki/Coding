4319219192af26758419eb430955fae21a92baef
Ivan Tseng <73100384+Yuma-no-yuki@users.noreply.github.com>
2024-01-08 16:30:33 +0800

    Create 1137. N-th Tribonacci Number.py

---
+++ DP/1137. N-th Tribonacci Number.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def tribonacci(self, n):
+        dp=[0,1,1]
+        if n>=3:
+            for i in range(3,n+1):
+                dp.append(dp[i-1]+dp[i-2]+dp[i-3])
+        return dp[n]
