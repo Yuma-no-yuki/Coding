0b0917696c3536c211b5c10d4409aac4169fe927
Ivan Tseng <73100384+Yuma-no-yuki@users.noreply.github.com>
2024-02-15 18:30:24 +0800

    Update 10921.py

--- UVA/10921.py
+++ UVA/10921.py
@@ -1,50 +1,48 @@
-ans=[]
+set = {
+    'A':2,
+    'B':2,
+    'C':2,
+    'D':3,
+    'E':3,
+    'F':3,
+    'G':4,
+    'H':4,
+    'I':4,
+    'J':5,
+    'K':5,
+    'L':5,
+    'M':6,
+    'N':6,
+    'O':6,
+    'P':7,
+    'Q':7,
+    'R':7,
+    'S':7,
+    'T':8,
+    'U':8,
+    'V':8,
+    'W':9,
+    'X':9,
+    'Y':9,
+    'Z':9
+}
+ans = []
 try:
     while True:
-        a=input()
-        count1=0
-        count2=0
-        change=[]
-        for i in a:
-            for j in range(65,90):
-                if chr(j)==i:
-                    count1+=1
-        d={
-            "A":2,
-            "B":2,
-            "C":2,
-            "D":3,
-            "E":3,
-            "F":3,
-            "G":4,
-            "H":4,
-            "I":4,
-            "J":5,
-            "K":5,
-            "L":5,
-            "M":6,
-            "N":6,
-            "O":6,
-            "P":7,
-            "Q":7,
-            "R":7,
-            "S":7,
-            "T":8,
-            "U":8,
-            "V":8,
-            "W":9,
-            "X":9,
-            "Y":9,
-            "Z":9
-        }
-        for i in a:
-            if i in d.keys():
-                change.append(d.get(i))
+        s=input()
+        s2 = ''
+        count1 = 0
+        count2 = 0
+        for x in s:
+            if x in list(set.keys()):
+                s2+=str(set[x])
             else:
-                change.append(i)
-        t="".join(map(str,change))
-        ans.append(t)
+                s2+=x
+            if x == '-':
+                count2+=1
+            else:
+                count1 += 1
+        ans.append(s2)
 except EOFError:
     pass
-for get in ans:
-    print(get)
+[print(x)for x in ans]
