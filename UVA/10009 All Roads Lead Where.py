105e6e90ca21a56a7aeed0fb4833a1f2b82cffcb
Ivan Tseng <73100384+Yuma-no-yuki@users.noreply.github.com>
2024-03-12 17:40:05 +0800

    Update 10009 All Roads Lead Where.py

--- UVA/10009 All Roads Lead Where.py
+++ UVA/10009 All Roads Lead Where.py
@@ -17,22 +17,22 @@ for _ in range(int(input())):
         a2 = a2[0]
         s1 = a1
         s2 = a2
-        while a1 != 'R':
+        while a1 != 'R': #向頂層尋找
             for k in dic:
                 if a1 in dic[k]:
                     a1 = k
                     s1 += a1
-        while a2 != 'R':
-            for k in dic:
+        while a2 != 'R': #向頂層尋找
+            for k in dic: 
                 if a2 in dic[k]:
                     a2 = k
                     s2 += a2
         count = 0
-        for i in range(min(len(s1), len(s2))):
+        for i in range(min(len(s1), len(s2))): #計算將s1,s2相同元素數量
             if s1[::-1][i] == s2[::-1][i]:
                 count += 1
         s2 = s2[::-1]
-        if count - 1 == 0:
+        if count - 1 == 0: #因list中[:-0]時會出錯->需進行判斷
             ans.append(s1+s2[count:])
         else:
             ans.append(s1[:-(count-1)] + s2[count:])
